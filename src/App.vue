bfd916d36ead839048b8e6fdb9dce8ba522c89ee
fosabdul <um0364nu@go.minnstate.edu>
2021-12-08 00:33:57 -0600

    done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- any writing here will show the app display -->
+  <div id="app">
+
+    <!-- v-bind to prop name in component
+Value is data in this component
+ -->
+    <would-you-rather v-bind:question="wyrQuestion"
+      v-bind:answer1="wyrAnswer1"
+      v-bind:answer2="wyrAnswer2"
+      v-on:answer-changed="answerChanged"></would-you-rather>
+
+    <p>{{ userSelectionMessage }}</p>
+    
+
+    
+    
+   
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import WouldYouRather from './components/WouldYouRather.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },// data needs to be a function that returns the component's data 
+
+  data() {
+    return {
+      wyrQuestion: ' Would you rather ride a camel or ride a horse?',
+      wyrAnswer1: 'Ride a Camel',
+      wyrAnswer2: 'Ride a Horse',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! You chose ${choice}`
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: rgb(181, 181, 165);
+} 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: rgb(29, 26, 26);
+  margin: 60px;
+  padding: 60px;
+  margin-top: 40px;
+  background: rgb(179, 225, 196);
+}
+p {
+  font-style: italic;
 }
 </style>
